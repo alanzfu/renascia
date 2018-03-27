260893fc1da2cbbb9f21f992cf1af012ce508565
Alan Fu <alanfu@Alans-MacBook-Pro.local>
2018-03-26 18:53:47 -0700

    get current and remove OUR FIRM

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+var main=function(){
+  $('navbar').html('<div class="row nav"><div class="container"><div class="col-sm-4"><a href="index.html"><img class="header-logo" src="img/logo.png"></a></div><div class="col-sm-8 "><ul><li><a href="index.html">HOME</a></li><li><a href="team.html">TEAM</a></li><li><a href="specialization.html">SERVICES</a></li><li><a href="contact.html">CONTACT</a></li></ul></div></div></div>')
+  $('footer').html('<div class="row footer"><div class="col-sm-12"><p>Copyright Renascia. All rights reserved.</p></div></div>')
+};
+
+$(document).ready(main);
